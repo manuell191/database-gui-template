f3c13878995311f588c0534b637e50f6b3d571e0
manuell191 <60369853+manuell191@users.noreply.github.com>
2022-07-19 19:12:13 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,25 @@ Each line is 25 pixels in height
 Each letter is 8 pixels in length
 '''
 
-database_dict = {
+def login(username, password):
+    database_dict = {
 
-}
+    }
+
+    f = open("database.txt", "r")
+    database_reference = f.readlines()
+
+    for i in database_reference:
+        split_data = i.split(";")
+        database_dict[split_data[0]] = split_data[1]
+    
+    if username in database_dict:
+        if database_dict[username] == password:
+            print("You logged in!\n")
+        else:
+            print("Username or password invalid.")
+    else:
+        print("Username or password invalid.")
 
 def signUp(username, password):
     new_user_list = [username, ";", password, ";", "\n"]
@@ -31,7 +47,7 @@ def loginSetup():
     backButton = Button(window, text="Back", width=10, command=lambda: [userText.destroy(), userBox.destroy(), passText.destroy(), passBox.destroy(), backButton.destroy(), enterButton.destroy(), exitButton.destroy(), menu()])
     backButton.place(x=25, y=404)
 
-    enterButton = Button(window, text="Enter", width=10, command=lambda: [signUp(userBox.get(), passBox.get())])
+    enterButton = Button(window, text="Enter", width=10, command=lambda: [login(userBox.get(), passBox.get())])
     enterButton.place(x=225, y=225)
 
     exitButton = Button(text="Exit", width=10, command=window.destroy)
